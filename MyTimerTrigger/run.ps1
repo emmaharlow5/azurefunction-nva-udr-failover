ff823b0f39b01015ad8277f9981b45d43a74284a
emmaharlow5 <96620371+emmaharlow5@users.noreply.github.com>
2024-09-21 11:33:00 +0100

    Update run.ps1

--- MyTimerTrigger/run.ps1
+++ MyTimerTrigger/run.ps1
@@ -92,258 +92,258 @@ $IntSleep = $env:FWDELAY       # Delay in seconds between tries
 #     catch { Send-MailMessage -SmtpServer $MailServers[2] -From $MailFrom -To $MailTo -Subject $Mesage -Body $Message }
 # }
 
-# Function Test-VMStatus ($VM, $FWResourceGroup) 
-# {
-#   $VMDetail = Get-AzVM -ResourceGroupName $FWResourceGroup -Name $VM -Status
-#   foreach ($VMStatus in $VMDetail.Statuses)
-#   { 
-#     $Status = $VMStatus.code
+Function Test-VMStatus ($VM, $FWResourceGroup) 
+{
+  $VMDetail = Get-AzVM -ResourceGroupName $FWResourceGroup -Name $VM -Status
+  foreach ($VMStatus in $VMDetail.Statuses)
+  { 
+    $Status = $VMStatus.code
       
-#     if($Status.CompareTo('PowerState/running') -eq 0)
-#     {
-#       Return $False
-#     }
-#   }
-#   Return $True  
-# }
+    if($Status.CompareTo('PowerState/running') -eq 0)
+    {
+      Return $False
+    }
+  }
+  Return $True  
+}
 
-# Function Test-TCPPort ($Server, $Port)
-# {
-#   $TCPClient = New-Object -TypeName system.Net.Sockets.TcpClient
-#   $Iar = $TCPClient.BeginConnect($Server, $Port, $Null, $Null)
-#   $Wait = $Iar.AsyncWaitHandle.WaitOne(1000, $False)
-#   return $Wait
-# }
+Function Test-TCPPort ($Server, $Port)
+{
+  $TCPClient = New-Object -TypeName system.Net.Sockets.TcpClient
+  $Iar = $TCPClient.BeginConnect($Server, $Port, $Null, $Null)
+  $Wait = $Iar.AsyncWaitHandle.WaitOne(1000, $False)
+  return $Wait
+}
 
-# Function Start-Failover 
-# {
-#   foreach ($SubscriptionID in $Script:ListOfSubscriptionIDs){
-#     Set-AzContext -SubscriptionId $SubscriptionID
-#     $RTable = @()
-#     $TagValue = $env:FWUDRTAG
-#     $Res = Find-AzResource -TagName nva_ha_udr -TagValue $TagValue
+Function Start-Failover 
+{
+  foreach ($SubscriptionID in $Script:ListOfSubscriptionIDs){
+    Set-AzContext -SubscriptionId $SubscriptionID
+    $RTable = @()
+    $TagValue = $env:FWUDRTAG
+    $Res = Find-AzResource -TagName nva_ha_udr -TagValue $TagValue
 
-#     foreach ($RTable in $Res)
-#     {
-#       $Table = Get-AzRouteTable -ResourceGroupName $RTable.ResourceGroupName -Name $RTable.Name
+    foreach ($RTable in $Res)
+    {
+      $Table = Get-AzRouteTable -ResourceGroupName $RTable.ResourceGroupName -Name $RTable.Name
       
-#       foreach ($RouteName in $Table.Routes)
-#       {
-#         Write-Output -InputObject "Updating route table..."
-#         Write-Output -InputObject $RTable.Name
+      foreach ($RouteName in $Table.Routes)
+      {
+        Write-Output -InputObject "Updating route table..."
+        Write-Output -InputObject $RTable.Name
 
-#         for ($i = 0; $i -lt $PrimaryInts.count; $i++)
-#         {
-#           if($RouteName.NextHopIpAddress -eq $SecondaryInts[$i])
-#           {
-#             Write-Output -InputObject 'Secondary NVA is already ACTIVE' 
+        for ($i = 0; $i -lt $PrimaryInts.count; $i++)
+        {
+          if($RouteName.NextHopIpAddress -eq $SecondaryInts[$i])
+          {
+            Write-Output -InputObject 'Secondary NVA is already ACTIVE' 
             
-#           }
-#           elseif($RouteName.NextHopIpAddress -eq $PrimaryInts[$i])
-#           {
-#             Set-AzRouteConfig -Name $RouteName.Name  -NextHopType VirtualAppliance -RouteTable $Table -AddressPrefix $RouteName.AddressPrefix -NextHopIpAddress $SecondaryInts[$i] 
-#           }
-#         }
+          }
+          elseif($RouteName.NextHopIpAddress -eq $PrimaryInts[$i])
+          {
+            Set-AzRouteConfig -Name $RouteName.Name  -NextHopType VirtualAppliance -RouteTable $Table -AddressPrefix $RouteName.AddressPrefix -NextHopIpAddress $SecondaryInts[$i] 
+          }
+        }
 
-#       }
+      }
   
-#       $UpdateTable = [scriptblock]{param($Table) Set-AzRouteTable -RouteTable $Table}
-#       &$UpdateTable $Table
+      $UpdateTable = [scriptblock]{param($Table) Set-AzRouteTable -RouteTable $Table}
+      &$UpdateTable $Table
 
-#     }
-#   }
+    }
+  }
 
-#   # Send-AlertMessage -message "NVA Alert: Failover to Secondary FW2"
+  # Send-AlertMessage -message "NVA Alert: Failover to Secondary FW2"
 
-# }
+}
 
-# Function Start-Failback 
-# {
-#   foreach ($SubscriptionID in $Script:ListOfSubscriptionIDs)
-#   {
-#     Set-AzContext -SubscriptionId $SubscriptionID
-#     $TagValue = $env:FWUDRTAG
-#     $Res = Find-AzResource -TagName nva_ha_udr -TagValue $TagValue
+Function Start-Failback 
+{
+  foreach ($SubscriptionID in $Script:ListOfSubscriptionIDs)
+  {
+    Set-AzContext -SubscriptionId $SubscriptionID
+    $TagValue = $env:FWUDRTAG
+    $Res = Find-AzResource -TagName nva_ha_udr -TagValue $TagValue
 
-#     foreach ($RTable in $Res)
-#     {
-#       $Table = Get-AzRouteTable -ResourceGroupName $RTable.ResourceGroupName -Name $RTable.Name
+    foreach ($RTable in $Res)
+    {
+      $Table = Get-AzRouteTable -ResourceGroupName $RTable.ResourceGroupName -Name $RTable.Name
 
-#       foreach ($RouteName in $Table.Routes)
-#       {
-#         Write-Output -InputObject "Updating route table..."
-#         Write-Output -InputObject $RTable.Name
+      foreach ($RouteName in $Table.Routes)
+      {
+        Write-Output -InputObject "Updating route table..."
+        Write-Output -InputObject $RTable.Name
 
-#         for ($i = 0; $i -lt $PrimaryInts.count; $i++)
-#         {
-#           if($RouteName.NextHopIpAddress -eq $PrimaryInts[$i])
-#           {
-#             Write-Output -InputObject 'Primary NVA is already ACTIVE' 
+        for ($i = 0; $i -lt $PrimaryInts.count; $i++)
+        {
+          if($RouteName.NextHopIpAddress -eq $PrimaryInts[$i])
+          {
+            Write-Output -InputObject 'Primary NVA is already ACTIVE' 
           
-#           }
-#           elseif($RouteName.NextHopIpAddress -eq $SecondaryInts[$i])
-#           {
-#             Set-AzRouteConfig -Name $RouteName.Name  -NextHopType VirtualAppliance -RouteTable $Table -AddressPrefix $RouteName.AddressPrefix -NextHopIpAddress $PrimaryInts[$i]
-#           }  
-#         }
+          }
+          elseif($RouteName.NextHopIpAddress -eq $SecondaryInts[$i])
+          {
+            Set-AzRouteConfig -Name $RouteName.Name  -NextHopType VirtualAppliance -RouteTable $Table -AddressPrefix $RouteName.AddressPrefix -NextHopIpAddress $PrimaryInts[$i]
+          }  
+        }
 
-#       }  
+      }  
 
-#       $UpdateTable = [scriptblock]{param($Table) Set-AzRouteTable -RouteTable $Table}
-#       &$UpdateTable $Table 
+      $UpdateTable = [scriptblock]{param($Table) Set-AzRouteTable -RouteTable $Table}
+      &$UpdateTable $Table 
 
-#     }
-#   }
+    }
+  }
 
-#   # Send-AlertMessage -message "NVA Alert: Failback to Primary FW1"
+  # Send-AlertMessage -message "NVA Alert: Failback to Primary FW1"
 
-# }
+}
 
-# Function Get-FWInterfaces
-# {
-#   $Nics = Get-AzNetworkInterface | Where-Object -Property VirtualMachine -NE -Value $Null
-#   $VMS1 = Get-AzVM -Name $VMFW1Name -ResourceGroupName $FW1RGName
-#   $VMS2 = Get-AzVM -Name $VMFW2Name -ResourceGroupName $FW2RGName
+Function Get-FWInterfaces
+{
+  $Nics = Get-AzNetworkInterface | Where-Object -Property VirtualMachine -NE -Value $Null
+  $VMS1 = Get-AzVM -Name $VMFW1Name -ResourceGroupName $FW1RGName
+  $VMS2 = Get-AzVM -Name $VMFW2Name -ResourceGroupName $FW2RGName
 
-#   foreach($Nic in $Nics)
-#   {
+  foreach($Nic in $Nics)
+  {
 
-#     if (($Nic.VirtualMachine.Id -EQ $VMS1.Id) -Or ($Nic.VirtualMachine.Id -EQ $VMS2.Id)) 
-#     {
-#       $VM = $VMS | Where-Object -Property Id -EQ -Value $Nic.VirtualMachine.Id
-#       $Prv = $Nic.IpConfigurations | Select-Object -ExpandProperty PrivateIpAddress  
+    if (($Nic.VirtualMachine.Id -EQ $VMS1.Id) -Or ($Nic.VirtualMachine.Id -EQ $VMS2.Id)) 
+    {
+      $VM = $VMS | Where-Object -Property Id -EQ -Value $Nic.VirtualMachine.Id
+      $Prv = $Nic.IpConfigurations | Select-Object -ExpandProperty PrivateIpAddress  
 
-#       if ($VM.Name -eq $VMFW1Name)
-#       {
-#         $Script:PrimaryInts += $Prv
-#       }
-#       elseif($VM.Name -eq $vmFW2Name)
-#       {
-#         $Script:SecondaryInts += $Prv
-#       }
+      if ($VM.Name -eq $VMFW1Name)
+      {
+        $Script:PrimaryInts += $Prv
+      }
+      elseif($VM.Name -eq $vmFW2Name)
+      {
+        $Script:SecondaryInts += $Prv
+      }
 
-#     }
+    }
 
-#   }
-# }
+  }
+}
 
-# Function Get-Subscriptions
-# {
-#   Write-Output -InputObject "Enumerating all subscriptins ..."
-#   $Script:ListOfSubscriptionIDs = (Get-AzSubscription).SubscriptionId
-#   Write-Output -InputObject $Script:ListOfSubscriptionIDs
-# }
+Function Get-Subscriptions
+{
+  Write-Output -InputObject "Enumerating all subscriptins ..."
+  $Script:ListOfSubscriptionIDs = (Get-AzSubscription).SubscriptionId
+  Write-Output -InputObject $Script:ListOfSubscriptionIDs
+}
 
-# #--------------------------------------------------------------------------
-# # Main code block for Azure function app                       
-# #--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
+# Main code block for Azure function app                       
+#--------------------------------------------------------------------------
 
-# #$Password = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force
-# #$Credential = New-Object System.Management.Automation.PSCredential ($env:SP_USERNAME, $Password)
-# #$AzureEnv = Get-AzEnvironment -Name $env:AZURECLOUD
-# #Add-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $Credential -SubscriptionId $env:SUBSCRIPTIONID -Environment $AzureEnv
+#$Password = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force
+#$Credential = New-Object System.Management.Automation.PSCredential ($env:SP_USERNAME, $Password)
+#$AzureEnv = Get-AzEnvironment -Name $env:AZURECLOUD
+#Add-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $Credential -SubscriptionId $env:SUBSCRIPTIONID -Environment $AzureEnv
 
-# #$Context = Get-AzContext
-# #Set-AzContext -Context $Context
+#$Context = Get-AzContext
+#Set-AzContext -Context $Context
 
-# #--------------------------------------------------------------------------
-# # Use Managed Identity                   
-# #--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
+# Use Managed Identity                   
+#--------------------------------------------------------------------------
 
 Connect-AzAccount -Identity
 
-# $Script:PrimaryInts = @()
-# $Script:SecondaryInts = @()
-# $Script:ListOfSubscriptionIDs = @()
+$Script:PrimaryInts = @()
+$Script:SecondaryInts = @()
+$Script:ListOfSubscriptionIDs = @()
 
-# # Check NVA firewall status $intTries with $intSleep between tries
+# Check NVA firewall status $intTries with $intSleep between tries
 
-# $CtrFW1 = 0
-# $CtrFW2 = 0
-# $FW1Down = $True
-# $FW2Down = $True
+$CtrFW1 = 0
+$CtrFW2 = 0
+$FW1Down = $True
+$FW2Down = $True
 
-# $VMS = Get-AzVM
+$VMS = Get-AzVM
 
-# Get-Subscriptions
-# Get-FWInterfaces
+Get-Subscriptions
+Get-FWInterfaces
 
-# # Test primary and secondary NVA firewall status 
+# Test primary and secondary NVA firewall status 
 
-# For ($Ctr = 1; $Ctr -le $IntTries; $Ctr++)
-# {
+For ($Ctr = 1; $Ctr -le $IntTries; $Ctr++)
+{
   
-#   if ($Monitor -eq 'VMStatus')
-#   {
-#     $FW1Down = Test-VMStatus -VM $VMFW1Name -FwResourceGroup $FW1RGName
-#     $FW2Down = Test-VMStatus -VM $VMFW2Name -FwResourceGroup $FW2RGName
-#   }
+  if ($Monitor -eq 'VMStatus')
+  {
+    $FW1Down = Test-VMStatus -VM $VMFW1Name -FwResourceGroup $FW1RGName
+    $FW2Down = Test-VMStatus -VM $VMFW2Name -FwResourceGroup $FW2RGName
+  }
 
-#   if ($Monitor -eq 'TCPPort')
-#   {
-#     $FW1Down = -not (Test-TCPPort -Server $TCPFW1Server -Port $TCPFW1Port)
-#     $FW2Down = -not (Test-TCPPort -Server $TCPFW2Server -Port $TCPFW2Port)
-#   }
+  if ($Monitor -eq 'TCPPort')
+  {
+    $FW1Down = -not (Test-TCPPort -Server $TCPFW1Server -Port $TCPFW1Port)
+    $FW2Down = -not (Test-TCPPort -Server $TCPFW2Server -Port $TCPFW2Port)
+  }
 
-#   Write-Output -InputObject "Pass $Ctr of $IntTries - FW1Down is $FW1Down, FW2Down is $FW2Down"
+  Write-Output -InputObject "Pass $Ctr of $IntTries - FW1Down is $FW1Down, FW2Down is $FW2Down"
 
-#   if ($FW1Down) 
-#   {
-#     $CtrFW1++
-#   }
+  if ($FW1Down) 
+  {
+    $CtrFW1++
+  }
 
-#   if ($FW2Down) 
-#   {
-#     $CtrFW2++
-#   }
+  if ($FW2Down) 
+  {
+    $CtrFW2++
+  }
 
-#   Write-Output -InputObject "Sleeping $IntSleep seconds"
-#   Start-Sleep $IntSleep
-# }
+  Write-Output -InputObject "Sleeping $IntSleep seconds"
+  Start-Sleep $IntSleep
+}
 
-# # Reset individual test status and determine overall NVA firewall status
+# Reset individual test status and determine overall NVA firewall status
 
-# $FW1Down = $False
-# $FW2Down = $False
+$FW1Down = $False
+$FW2Down = $False
 
-# if ($CtrFW1 -eq $intTries) 
-# {
-#   $FW1Down = $True
-# }
+if ($CtrFW1 -eq $intTries) 
+{
+  $FW1Down = $True
+}
 
-# if ($CtrFW2 -eq $intTries) 
-# {
-#   $FW2Down = $True
-# }
+if ($CtrFW2 -eq $intTries) 
+{
+  $FW2Down = $True
+}
 
-# # Failover or failback if needed
+# Failover or failback if needed
 
-# if (($FW1Down) -and -not ($FW2Down))
-# {
-#   if ($FailOver)
-#   {
-#     Write-Output -InputObject 'FW1 Down - Failing over to FW2'
-#     Start-Failover 
-#   }
-# }
-# elseif (-not ($FW1Down) -and ($FW2Down))
-# {
-#   if ($FailBack)
-#   {
-#     Write-Output -InputObject 'FW2 Down - Failing back to FW1'
-#     Start-Failback
-#   }
-#   else 
-#   {
-#     Write-Output -InputObject 'FW2 Down - Failing back disabled'
-#   }
-# }
-# elseif (($FW1Down) -and ($FW2Down))
-# {
-#   Write-Output -InputObject 'Both FW1 and FW2 Down - Manual recovery action required'
-#   # Send-AlertMessage -message "NVA Alert: Both FW1 and FW2 Down - Manual recovery action is required"
-# }
-# else
-# {
-#   Write-Output -InputObject 'Both FW1 and FW2 Up - No action is required'
-# }
+if (($FW1Down) -and -not ($FW2Down))
+{
+  if ($FailOver)
+  {
+    Write-Output -InputObject 'FW1 Down - Failing over to FW2'
+    Start-Failover 
+  }
+}
+elseif (-not ($FW1Down) -and ($FW2Down))
+{
+  if ($FailBack)
+  {
+    Write-Output -InputObject 'FW2 Down - Failing back to FW1'
+    Start-Failback
+  }
+  else 
+  {
+    Write-Output -InputObject 'FW2 Down - Failing back disabled'
+  }
+}
+elseif (($FW1Down) -and ($FW2Down))
+{
+  Write-Output -InputObject 'Both FW1 and FW2 Down - Manual recovery action required'
+  # Send-AlertMessage -message "NVA Alert: Both FW1 and FW2 Down - Manual recovery action is required"
+}
+else
+{
+  Write-Output -InputObject 'Both FW1 and FW2 Up - No action is required'
+}
