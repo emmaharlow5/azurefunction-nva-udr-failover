327f365fcb9dcb9bbd45443ede82cca88e96f149
emmaharlow5 <96620371+emmaharlow5@users.noreply.github.com>
2025-01-09 16:06:17 +0000

    Update run.ps1

--- vMXHATrigger/run.ps1
+++ vMXHATrigger/run.ps1
@@ -326,10 +326,10 @@ Get-FWInterfaces
 For ($Ctr = 1; $Ctr -le $IntTries; $Ctr++) {
   
   if ($Monitor -eq 'VMStatus') {
-    # $vMX1Down = Test-VMStatus -VM $VMX1VMName -vMXResourceGroup $vMX1RGName
-    $vMX1Down = $True
-    $vMX2Down = Test-VMStatus -VM $VMX2VMName -vMXResourceGroup $vMX2RGName
-    # $vMX2Down = $True
+    $vMX1Down = Test-VMStatus -VM $VMX1VMName -vMXResourceGroup $vMX1RGName
+    # $vMX1Down = $True
+    # $vMX2Down = Test-VMStatus -VM $VMX2VMName -vMXResourceGroup $vMX2RGName
+    $vMX2Down = $True
   }
 
   if ($Monitor -eq 'TCPPort') {
